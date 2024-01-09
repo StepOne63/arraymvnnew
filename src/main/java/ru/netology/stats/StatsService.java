460244dc0d691d317c93c7508aeaf94015fd1711
Step X <stepan170297@gmail.com>
2024-01-09 10:33:08 +0400

    first commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,69 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public long sum(long[] sales) {
+        long totalsale = 0;
+        for (long sale : sales) {
+            totalsale += sale;
+        }
+        return totalsale;
+    }
+
+    public long average(long[] sales) {
+        long totalsale = 0;
+        for (long sale : sales) {
+            totalsale += sale;
+        }
+        long averagesale = totalsale / 12;
+        return averagesale;
+    }
+
+    public int calcMonthMaximumSale(long[] sales) {
+        int monthMaximum = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[monthMaximum] < sales[i]) {
+                monthMaximum = i;
+            }
+        }
+        return monthMaximum + 1;
+    }
+
+    public int calcMonthMinimumSale(long[] sales) {
+        int monthMinimum = 0;
+        for (int i = 0; i > sales.length; i++) {
+            if (sales[monthMinimum] < sales[i]) {
+                monthMinimum = i;
+            }
+        }
+        return monthMinimum + 1;
+    }
+
+
+    public int calcMonthMoreAverageSale(long[] sales) {
+        int count = 0;
+        long averageSale = average(sales);
+        for (long sale : sales) {
+            if (sale > averageSale ) {
+                count++;
+
+            }
+        }
+        return count;
+
+    }
+
+
+
+    public int calcMonthBelowAverageSale(long[] sales) {
+        int count = 0;
+        long averageSale = average(sales);
+        for (long sale : sales) {
+            if (sale < averageSale ) {
+                count++;
+
+            }
+        }
+        return count;
+
+    }
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,99 @@
+package ru.netology.stats;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class StatsServiceTest {
+    @Test
+    public void testSum() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+
+        assertEquals(actual, expected);
+    }
+
+
+    @Test
+    public void testAverage() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+        long actual = service.average(sales);
+        long expected = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / 12;
+        assertEquals(expected,actual);
+
+
+
+    }
+
+
+
+
+
+    @Test
+    public void testMounthMaximum() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+
+        long actual = service.calcMonthMaximumSale(sales);
+        long expected = 6;
+        assertEquals(expected, actual);
+
+    }
+
+
+
+    @Test
+    public void testMounthMinimum() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+
+        long actual = service.calcMonthMinimumSale(sales);
+        long expected = 1;
+        assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void testMonthMoreAverage() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+
+        long actual = service.calcMonthMoreAverageSale(sales);
+        long expected = 5;
+        assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void testMonthBelowAverage() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+
+        long actual = service.calcMonthBelowAverageSale(sales);
+        long expected = 5;
+        assertEquals(expected, actual);
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
